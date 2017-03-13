27d19d70f9ec5f34b99abe91d3ede27c396c6101
KamilKucharski <kamil.kucharski@intive.com>
2017-03-13 21:25:54 +0100

    MainViewModel and DataBinding.

--- app/src/main/java/net/fezzed/mvvmdiffutil/MainActivity.java
+++ app/src/main/java/net/fezzed/mvvmdiffutil/MainActivity.java
@@ -1,13 +1,18 @@
 package net.fezzed.mvvmdiffutil;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import net.fezzed.mvvmdiffutil.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding viewDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        viewDataBinding.setViewModel(new MainViewModel());
     }
 }
---
+++ app/src/main/java/net/fezzed/mvvmdiffutil/MainViewModel.java
@@ -0,0 +1,18 @@
+package net.fezzed.mvvmdiffutil;
+
+
+public class MainViewModel {
+
+    public void onSwitchEventClick() {
+
+    }
+
+    public void onSwitchOddClick() {
+
+    }
+
+    public void onAddMoreItemsClick() {
+
+    }
+
+}
